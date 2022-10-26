1ee9ca6fd4116e476d5e2c605ad887c2e4f0ac08
sudha123 <sudhanaukala1997@gmail.com>
2022-10-26 20:22:25 +0530

    reporting die

--- src/com/bl/SnakesAndLadders.java
+++ src/com/bl/SnakesAndLadders.java
@@ -6,26 +6,32 @@ public class SnakesAndLadders {
     static final int IF_LADDER = 1;
     static final int IF_SNAKE = 2;
     static int currentPosition = 0;
+    static int die = 0;
 
     public static void main(String[] args) {
         System.out.println("Welcome to Snakes and Ladders Game");
-        int dieNum = (int) (Math.random() * 10) % 6 + 1;
-        int option = (int) (Math.random() * 10) % 3;
-        int dienum = 0;
-        if (option == IF_LADDER) {
-            currentPosition += dieNum;
-        } else if (option == IF_SNAKE) {
-            currentPosition -= dieNum;
-        } else if (option == NO_PLAY) {
-            System.out.println("No Play");
+        while (currentPosition < 100) {
+            int dieNum = (int) (Math.random() * 10) % 6 + 1;
+            int option = (int) (Math.random() * 10) % 3;
+            int die = 0;
+            if (option == IF_LADDER) {
+                currentPosition += dieNum;
+            } else if (option == IF_SNAKE) {
+                currentPosition -= dieNum;
+            } else if (option == NO_PLAY) {
+                System.out.println("No Play");
+            }
+            if (currentPosition < START_POSITION) {
+                currentPosition = 0;
+            }
+            if (currentPosition > 100) {
+                currentPosition -= dieNum;
+            }
+            System.out.println("currentPosition is : " + currentPosition);
+            die++;
         }
-        if (currentPosition < START_POSITION) {
-            currentPosition = 0;
-        }
-        if (currentPosition > 100) {
-            currentPosition -= dieNum;
-        }
-        System.out.println(currentPosition);
+        System.out.println("Total number of times die rolled are : " + die);
     }
+
 }
 
